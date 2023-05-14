af639341edab5127cb11e0c20ab49581cb8f7aed
Kolawole Moses <xoloyinks@gmail.com>
2023-05-14 11:13:22 -0700

    First commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './output.css';
+import Page from './pages/page';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Page />
   </React.StrictMode>
 );
 
