3c44d8db420c00ba3bd98d393a89b1221ea67979
Kolawole Moses <xoloyinks@gmail.com>
2023-05-14 14:32:34 -0700

    Commit

--- src/pages/page.js
+++ src/pages/page.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import useScrollSnap from 'react-use-scroll-snap'
+import { useRef } from 'react'
+import Banner from './banner'
+
 
 export default function Page() {
+    const scrollRef = useRef(null);
+    useScrollSnap({ref:scrollRef, duration:10, delay:20});
   return (
     <>
-        <section>
-            Nothing yet!
+        <section ref={scrollRef}>
+            <Banner />
+            <section className='w-screen h-screen'></section>
+
         </section>
     </>
   )
