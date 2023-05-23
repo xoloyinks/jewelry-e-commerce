d4fdfd7e1f0d93fc89eb3a4ebb34936dc5fe8544
Kolawole Moses <xoloyinks@gmail.com>
2023-05-23 10:17:32 -0700

    Updated

--- src/pages/banner.js
+++ src/pages/banner.js
@@ -7,17 +7,17 @@ import { RxCaretDown } from 'react-icons/rx'
 export default function Banner() {
   return (
     <>
-        <section className='banner bg-slate-100 w-screen h-[98vh] overflow-hidden'>
+        <section className='banner w-screen h-[98vh] overflow-hidden'>
           <div className='exibit relative bg-black h-[92%] w-full'>
               <div className='__banner w-full h-full'></div>
               <div className='absolute bottom-[60px] text-white p-5 '>
                 <div className=' text-2xl tracking-widest font-thin'>LES FEMMES</div>
                 <br />
-                <button className='px-8 py-3 text-md bg-gray-700 tracking-widest'>NEW COLLECTION</button>
+                <button className='px-10 py-4 text-sm bg-gray-700 tracking-widest'>NEW COLLECTION</button>
               </div>
-              <div className='absolute -bottom-[50px] text-center  w-full text-white px-10 font-bold'>
+              <div className='absolute -bottom-[40px] text-center  w-full text-white px-10 font-bold'>
                 <button className='px-3 py-3 bg-slate-100 shadow-sm shadow-slate-500 rounded-full mx-3'>
-                  <RxCaretDown className='text-slate-500  text-5xl' />
+                  <RxCaretDown className='text-slate-500  text-4xl' />
                 </button>
               </div>
           </div>
