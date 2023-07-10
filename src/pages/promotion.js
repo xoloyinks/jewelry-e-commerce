6a7317f2c11d08b19dc922dbe3d2418123bdb080
Kolawole Moses <xoloyinks@gmail.com>
2023-07-09 19:38:03 -0700

    Updated

--- src/pages/promotion.js
+++ src/pages/promotion.js
@@ -9,15 +9,15 @@ export default function Promotion() {
             <marquee behavior="" direction="left" scrolldelay="1s" className="absolute top-0 px-5 py-[5px] text-black bg-white mt-2 z-50 text-[9px] lg:text-[10px] tracking-widest lg:mt-5 lg:py-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. A asperiores deleniti nulla. Saepe similique
              deleniti hic optio voluptate vitae deserunt quisquam a. Recusandae pariatur qui sit dicta consequuntur labore non?</marquee>
                 <div className='promotion_caption absolute p-5 tracking-wide max-[1020px]:flex max-[1020px]:items-center w-full h-full
-                 min-[800px]:flex min-[800px]:flex-col min-[800px]:justify-center min-[800px]:items-start min-[800px]:px-20
+                 lg:flex lg:flex-col lg:justify-center lg:items-start lg:px-20
                  '>
                     
-                    <h1 className='text-[14px] max-[400px]:text-[12px] text-white min-[750px]:w-[40%] w-[80%] lg:w-[50%] xl:w-[42%] lg:text-xl lg:tracking-widest min-[800px]:justify-left'>
+                    <h1 className='text-[14px] max-[400px]:text-[12px] text-white min-[750px]:w-[40%] w-[80%] lg:w-[50%] xl:w-[42%] lg:text-xl lg:tracking-widest lg:justify-left'>
                         EARRING IS NOT JUST AN ACCESSORY.
                         IT'S A REFLECTION OF A WOMAN'S 
                         PERSONALITY AND STYLE. 
                     </h1><br className='hidden min-[800px]:block'/><br className='hidden min-[800px]:block'/><br className='hidden min-[800px]:block'/>
-                    <h5 className='hidden min-[800px]:block min-[800px]:w-[37%] min-[800px]:text-xs min-[800px]:self-center min-[800px]:text-white min-[800px]:border-l-[2px] min-[800px]:border-gray-300 min-[800px]:p-5 '>
+                    <h5 className='hidden lg:w-[37%] lg:text-xs lg:self-center lg:text-white lg:border-l-[2px] lg:border-gray-300 lg:p-5 lg:block '>
                         From elegant necklaces and statement earrings to delicate
                         bracelets and stunning rings, our jewelry pieces are 
                         designed to make you shine.
@@ -30,7 +30,7 @@ export default function Promotion() {
                     </div>
                 </div>
             </div>
-            <div className='px-5 py-8 max-[400px]:py-5 text-sm max-[400px]:text-[12px] min-[750px]:text-center min-[750px]:mx-auto min-[750px]:w-[60%] min-[800px]:hidden'>
+            <div className='px-5 py-8 max-[400px]:py-5 text-sm max-[400px]:text-[12px] min-[750px]:text-center min-[750px]:mx-auto min-[750px]:w-[60%] min-[800px]:hidden lg:hidden'>
                 From elegant necklaces and statement earrings to delicate
                 bracelets and stunning rings, our jewelry pieces are 
                 designed to make you shine.
